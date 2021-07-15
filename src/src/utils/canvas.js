9c36ebbb11ec58e9248f4d3a8284d42b8ee9b565
tlmnn-adrn <adrian.tilmann@gmail.com>
2021-07-15 11:18:02 +0200

    Fixed the arrowhead

--- src/src/utils/canvas.js
+++ src/src/utils/canvas.js
@@ -16,7 +16,7 @@ const roundedRect = (c, x, y, w, h, radius, color = 'black') => {
     c.restore();
 }
 
-const arrow = (c, fromx, fromy, tox, toy, lineWidth = 2.5, headlen = 13) => {
+const arrow = (c, fromx, fromy, tox, toy, lineWidth = 2.5, headlen = 15) => {
 
     c.save();
 
@@ -27,13 +27,18 @@ const arrow = (c, fromx, fromy, tox, toy, lineWidth = 2.5, headlen = 13) => {
     var dx = tox - fromx;
     var dy = toy - fromy;
     var angle = Math.atan2(dy, dx);
+
     c.moveTo(fromx, fromy);
-    c.lineTo(tox, toy);
+    c.lineTo(
+        tox-headlen*Math.cos(angle)*Math.sqrt(3)/2, 
+        toy-headlen*Math.sin(angle)*Math.sqrt(3)/2
+        );
 
     c.stroke();
 
     c.beginPath();
 
+    c.moveTo(tox, toy);
     c.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
     c.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
     c.lineTo(tox, toy);
